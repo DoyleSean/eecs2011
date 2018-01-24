1a283c7ceadd2aaebe16eef12ced8e256c6c2de5
DoyleSean <doylesean@me.com>
2018-01-23 23:38:06 -0500

    finished dot() - added debug comments

--- A1/A1/src/A1Q1/SparseNumericVector.java
+++ A1/A1/src/A1Q1/SparseNumericVector.java
@@ -17,7 +17,7 @@ public class SparseNumericVector implements Iterable {
 
     protected SparseNumericNode head = null;
     protected SparseNumericNode tail = null;
-    protected long size;
+    protected long size = 0;
 
   /**
      * Iterator
@@ -50,49 +50,68 @@ public class SparseNumericVector implements Iterable {
   public void add(SparseNumericElement e) throws UnsupportedOperationException {
 	  // exception handling
       boolean doesExist = false;
-      while(this.iterator().hasNext()){										// traverse the elements
-    	  	if(this.iterator().next().getIndex() == e.getIndex()){				// check for existing indices
+      Iterator<SparseNumericElement> exceptionIterator = this.iterator();
+      while(exceptionIterator.hasNext()){											// traverse the elements
+    	  System.out.println("ADD 1ST WHILE");
+    	  	if(exceptionIterator.next().getIndex() == e.getIndex()){				// check for existing indices
     	  		doesExist = true;
     	  	}
       }	
-	  if(e.getValue() == 0 || doesExist){									// exception throwing
+	  if(e.getValue() == 0 || doesExist){										// exception throwing
 		  throw new UnsupportedOperationException();
-       }
-	  
+	  }
+	  System.out.println("EXCEPTIONS CLEARED");
 	  // functionality
 	  if(this.head == null){													// add to empty list
+		  System.out.println("ADDING TO EMPTY LIST CASE");
 		  SparseNumericNode n = new SparseNumericNode(e, null);
 		  this.head = n;
 		  this.tail = n;
+		  System.out.println("EMPTY LIST CASE CLEARED");
 	  } else if (e.getIndex() < this.getFirst().getElement().getIndex()){		// add to start of list
+		  System.out.println("ADDING TO HEAD OF LIST CASE");
 		  SparseNumericNode newHead = new SparseNumericNode(e, this.head);
 		  this.head = newHead;
-	  } else if (e.getIndex() > this.tail.getElement().getIndex()){			// add to end of list
+		  System.out.println("HEAD OF LIST CASE CLEARED");
+	  } else if (e.getIndex() > this.tail.getElement().getIndex()){				// add to end of list
+		  System.out.println("ADDING TO TAIL OF LIST CASE");
 		  SparseNumericNode newTail = new SparseNumericNode(e, null);
 		  this.tail.setNext(newTail);
 		  this.tail = newTail;
-	  } else {																// add to middle of list
-		  long insertIndex = e.getIndex();									// index of element to be inserted
-		  long lastNodeIndex, workingNodeIndex;								// indices of current and prev nodes
+		  System.out.println("TAIL OF LIST CASE CLEARED");
+	  } else {																	// add to middle of list
+		  long insertIndex = e.getIndex();										// index of element to be inserted
+		  long lastNodeIndex, workingNodeIndex;									// indices of current and prev nodes
 		  SparseNumericNode workingNode = this.head;							// current node
 		  SparseNumericNode lastNode = null;									// previous node
 		  SparseNumericNode insertNode = new SparseNumericNode(e, null);		// node to be inserted (temporarily null)
-		 
-		  while(this.iterator().hasNext()){									// traverse the vector searching for insert point
-			  workingNodeIndex = workingNode.getElement().getIndex();			// index of current node
+		  Iterator<SparseNumericElement> addIterator = this.iterator();
+		  while(addIterator.hasNext()){											// traverse the vector searching for insert point
+			  System.out.println("ADD general case WHILE");
+			  workingNodeIndex = workingNode.getElement().getIndex();			// index of current node's element
 			  if(workingNode.equals(this.head)){								// for when the insert is next to the head
-				  lastNodeIndex = 0;
-			  } else {														// for when the insert is anywhere else
+				  System.out.println("SET LASTNODE FOR HEADCHECK");
+				  lastNodeIndex = -1;
+			  } else {															// for when the insert is anywhere else
+				  System.out.println("SET LASTNODE FOR GENERAL CHECK");
 				  lastNodeIndex = lastNode.getElement().getIndex();
 			  }
 			  // check to see that the index to insert is less than the next and greater than the index before it
+			  System.out.println("CHECKING FOR INSERTION POINT!");
 			  if ((insertIndex < workingNodeIndex) && (insertIndex > lastNodeIndex)) {
-				  insertNode.setNext(workingNode);							// chain the inserted node to the next node
-				  lastNode.setNext(insertNode);								// chain the previous node to the inserted node
+				  System.out.println("CHAINING!");
+				  insertNode.setNext(workingNode);								// chain the inserted node to the next node
+				  lastNode.setNext(insertNode);									// chain the previous node to the inserted node
+				 
+				  break; //done!
 			  }
-			  lastNode = workingNode;										// the last node becomes the old working node
+			  System.out.println("NO POINT FOUND, MOVING ON...");
+			  lastNode = workingNode;											// the last node becomes the old working node
+			  workingNode = workingNode.getNext();
+			  addIterator.next();
 		  }
 	  }
+	  size++;
     }
 
     /**
@@ -109,22 +128,38 @@ public class SparseNumericVector implements Iterable {
     		SparseNumericNode lastNode = null;
     		SparseNumericNode workingNode = this.head;
     		SparseNumericNode nextNode = null;
-    		while(this.iterator().hasNext()){
-    			
+    		Iterator<SparseNumericElement> removeIterator = this.iterator();
+    		while(removeIterator.hasNext()){
+    			System.out.println("REM WHILE");
+    			nextNode = workingNode.getNext();
     			workingNodeIndex = workingNode.getElement().getIndex();			// track index of working node
     			if(workingNodeIndex == remIndex){								// check if indices match
     				if(workingNode.equals(this.head)){							// special case where head is removed
-    					this.head = this.head.getNext();							// update the head for this case
-    				} else {														// general case for all other removals
+    					System.out.println("REM HEAD CASE");
+    					this.head = this.head.getNext();						// update the head for this case
+    				} else if (workingNode.equals(this.tail)) {					// special case where 
+    					System.out.println("REM TAIL CASE");
+    					lastNode.setNext(null);
+    				} else {													// general case for all other removals
+    					System.out.println("REM GEN CASE");
     					lastNode.setNext(nextNode);								// chain around the working node
     				}
-    				return true;													// returns true since there was a removal
-    			} else {															// update nodes for next iteration
-    				lastNode = workingNode;
+    				System.out.println("REM SUCCESS REMOVED");
+    				this.size--;
+    				return true;												// returns true since there was a removal
+    			} else {														// update nodes for next iteration
+    				System.out.println("REM NONMATCH, NEXT ITERATION!");
+    				lastNode = workingNode;				
     				workingNode = nextNode;
-    				nextNode = nextNode.getNext();
+    				System.out.println("REM WORK ASSIGNMENT TO NEXT");
+    				if((lastNode == this.tail)) {
+    					System.out.println("TAIL HIT - BREAKING TO NONMATCH");
+    					break;
+    				}
     			}
+    			removeIterator.next();
     		}
+    		System.out.println("REM NONMATCH");
     		return false;														// returns false since it ran through the loop w/o a removal
     }
 
@@ -138,9 +173,50 @@ public class SparseNumericVector implements Iterable {
      */
 
     public double dot (SparseNumericVector Y) {
-        //implement this method
-        //this return statement is here to satisfy the compiler - replace it with your code.
-        return 0;
+    	System.out.println("DOT CALLED");
+    	Map<Long, SparseNumericElement> yMap = new LinkedHashMap<Long, SparseNumericElement>();
+    	Map<Long, SparseNumericElement> thisMap = new LinkedHashMap<Long, SparseNumericElement>();
+    	SparseNumericNode yNode = Y.head;
+    	SparseNumericNode thisNode = this.head; 
+    	System.out.println("DOT INSTANTIATION");
+    	System.out.println("THIS SIZE: " + this.getSize());
+    	for(int i = 0; i < this.getSize(); i++) {
+    		thisMap.put(thisNode.getElement().getIndex(), thisNode.getElement());
+    		thisNode = thisNode.getNext();
+    		System.out.println("DOT THIS MAPPING");
+    	}
+    	System.out.println("Y SIZE: " + Y.getSize());
+    	for(int y = 0; y < Y.getSize(); y++) {
+    		yMap.put(yNode.getElement().getIndex(), yNode.getElement());
+    		yNode = yNode.getNext();
+    		System.out.println("DOT Y MAPPED");
+    	}
+    	
+    	double dotSum = 0;
+    	if(thisMap.size() > yMap.size()) {
+    		System.out.println("DOT THIS LARGER THAN Y");
+    		for(long yIndex : yMap.keySet()) {
+    			System.out.println("DOT TRAVERSAL ON" + yIndex);
+    			if(thisMap.containsKey(yIndex)) {
+    				System.out.println("EQUALITY FOUND");
+    				dotSum += yMap.get(yIndex).getValue() * thisMap.get(yIndex).getValue();
+    				System.out.print(yMap.get(yIndex).getValue() + " x " + thisMap.get(yIndex).getValue());
+    			}
+    		}
+    	} else {
+    		System.out.println("DOT Y LARGER/EQUAL THAN THIS");
+    		System.out.println(thisMap.keySet());
+    		for(long thisIndex : thisMap.keySet()) {
+    			System.out.println("DOT TRAVERSAL ON" + thisIndex);
+    			if(yMap.containsKey(thisIndex)) {
+    				System.out.println("EQUALITY FOUND");
+    				dotSum += yMap.get(thisIndex).getValue() * thisMap.get(thisIndex).getValue();
+    				System.out.print(yMap.get(thisIndex).getValue() + " x " + thisMap.get(thisIndex).getValue());
+    			}
+    		}
+    	}
+        return dotSum;
+        
    }
 
        /**
--- A1/A1/src/A1Q1/testSparseNumericVector.java
+++ A1/A1/src/A1Q1/testSparseNumericVector.java
@@ -1,5 +1,5 @@
 package A1Q1;
-
+import java.util.*;
 /**
  *
  * @author jameselder
@@ -13,16 +13,25 @@ public class testSparseNumericVector {
         SparseNumericVector X = new SparseNumericVector();
         SparseNumericVector Y = new SparseNumericVector();
         double projection;
-
+        print(X);
         X.add(new SparseNumericElement(150000, 3.1415));
+        print(X);
         X.add(new SparseNumericElement(15, 3));
+        print(X);
         X.add(new SparseNumericElement(1500, 3.14));
+        print(X);
         X.add(new SparseNumericElement(150, 3.1));
+        print(X);
         X.add(new SparseNumericElement(15000, 3.141));
+        print(X);
         Y.add(new SparseNumericElement(150000, 1));
+        print(Y);
         Y.add(new SparseNumericElement(15, 1));
+        print(Y);
+        System.out.println("TESTER: PRINT OF X:" + X);
+        System.out.println("TESTER: PRINT OF Y:" + Y);
         X.remove((long) 150);
-
+        System.out.println("TESTER: PRINT OF X rem:" + X);
         projection = X.dot(Y);
 
         System.out.println("The inner product of");
@@ -32,5 +41,12 @@ public class testSparseNumericVector {
         System.out.println("is ");
         System.out.printf("%.5f\n\n",projection); //answer should be 3*1 + 3.1415*1 = 6.1415
      }
-
+    
+    public static void print(SparseNumericVector p) {
+    	Iterator<SparseNumericElement> priterator = p.iterator();
+    	while(priterator.hasNext()) {
+    		SparseNumericElement e = priterator.next();
+    		System.out.println("IDX: " + e.getIndex() + " VAL: " + e.getValue());
+    	}
+    }
 }
