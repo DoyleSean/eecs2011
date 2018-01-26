9c397124f7ecd6136c4bd3e05b63cf417168d779
DoyleSean <doylesean@me.com>
2018-01-25 23:13:07 -0500

    finished Q3

--- A1/A1/src/A1Q3/MaxStack.java
+++ A1/A1/src/A1Q3/MaxStack.java
@@ -6,40 +6,54 @@ import java.util.*;
  * a method for determining the maximum element on the stack in O(1) time.
  * @author jameselder
  */
-public class MaxStack<E extends Comparable> extends Stack<E> {
+public class MaxStack<E extends Comparable<E>> extends Stack<E> {
 
-    private Stack<E> maxStack;
-    private Stack<E> otherStack = new Stack<>();
+    private Stack<E> maxStack, unsortedStack;
     
     public MaxStack() {
         maxStack = new Stack<>();
+        unsortedStack = new Stack<>();
     }
     
     /* must run in O(1) time */
     public E push(E element) {
     		if(maxStack.isEmpty()){
+    			maxStack.push(element);								// simple push into empty stack			
+    			unsortedStack.push(element);
+    		} else if (element.compareTo(maxStack.peek()) < 0){		// element on top is more than whats being pushed
+    			maxStack.push(maxStack.peek());						// repush the max   			
+    			unsortedStack.push(element);						// always push for the unsorted stack
+    		} else if (element.compareTo(maxStack.peek()) >= 0){	// element on top is less than whats being pushed 
     			maxStack.push(element);
-    			otherStack.push(element);
-    		} else {
-    			
+    			unsortedStack.push(element);
     		}
-
-        return element; //Dummy return to satisfy compiler.  Remove once coded.
+        return element;
     }
 
     /* @exception  EmptyStackException  if this stack is empty. */
     /* must run in O(1) time */
    public synchronized E pop() {
-	   E obj = maxStack.peek();
-	   maxStack.pop();
-        return obj; //Dummy return to satisfy compiler.  Remove once coded.
+	   //exception handling
+	   if(unsortedStack.isEmpty()) {
+		   throw new EmptyStackException();
+	   } else { //functionality - pop from both stacks, return the max pop
+		   unsortedStack.pop();
+		   E obj = maxStack.peek();
+		   maxStack.pop();
+		   return obj;
+	   }
     }
 
     /* Returns the maximum value currenctly on the stack. */
     /* @exception  EmptyStackException  if this stack is empty. */
     /* must run in O(1) time */
     public synchronized E max() {
-    	
-        return null; //Dummy return to satisfy compiler.  Remove once coded.
+    	// exception handling
+    	if(unsortedStack.isEmpty()) {
+    		throw new EmptyStackException();
+    	}
+    	// functionality - top of maxstack is the highest element
+    	E max = maxStack.peek();
+        return max; //Dummy return to satisfy compiler.  Remove once coded.
     }
 }
