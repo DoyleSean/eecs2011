bd38e3daee9170025d25459c2876a04b231d51ca
DoyleSean <doylesean@me.com>
2018-01-25 21:37:18 -0500

    finished Q2

--- A1/A1/src/A1Q2/IntegralImage.java
+++ A1/A1/src/A1Q2/IntegralImage.java
@@ -9,9 +9,11 @@ package A1Q2;
  */
 public class IntegralImage {
 
+	
     private final int[][] integralImage;
     private final int imageHeight; // height of image (first index)
     private final int imageWidth; // width of image (second index)
+    private int topSum, leftSum, diagSum, directSum;
 
     /**
      * Constructs an integral image from the given input image.  
@@ -21,18 +23,46 @@ public class IntegralImage {
      * @throws InvalidImageException Thrown if input array is not rectangular
      */
     public IntegralImage(int[][] image) throws InvalidImageException {
-        this.imageHeight = image.length;					// set height
-        this.imageWidth = image[0].length;				// set width
-        for(int i = 0; i < this.imageHeight; i++){		// compare row lengths for equality
-        		if(this.imageWidth != image[i].length){		// if uneven, throw exception
+    	// exception handling
+        this.imageHeight = image.length;							// set height
+        this.imageWidth = image[0].length;							// set width
+        for(int i = 0; i < this.imageHeight; i++){					// compare row lengths for equality
+        		if(this.imageWidth != image[i].length){				// if uneven, throw exception
         			throw new InvalidImageException();
         		}
         }
+        
+        // summed area table functionality 
+        this.integralImage = new int[this.imageHeight][this.imageWidth];
         for(int i = 0; i < this.imageHeight; i++){
         		for(int j = 0; j < this.imageWidth; j++){
+        			boolean diagTop= false;							// track if theres room above 
+        			boolean diagLeft = false;						// track if theres room to the left
+        			this.directSum = image[i][j];					// direct value to add to the average from the image
+        			if(i==0) {										// check if we're on an edge
+        				this.topSum = 0;							// top edges have no top space to sum in
+        				this.diagSum = 0;							// edges dont have diagonal spaces
+        			} else {
+        				diagTop = true;								// theres room in the top dimension for a diagonal to exist
+        				this.topSum = this.integralImage[i-1][j];	// top sum is placed into the integral image
+        			}
+        			if(j==0) {										// check if we're on an edge
+        				this.leftSum = 0;
+        				this.diagSum = 0;
+        			} else {
+        				diagLeft = true;
+        				this.leftSum = this.integralImage[i][j-1];
+        			}
+        			if(diagLeft && diagTop) {							// if theres room in the vertical and horiz dimension
+        				this.diagSum = this.integralImage[i-1][j-1];	// factor in the diagonal into the sum
+        			} else {
+        				this.diagSum = 0;
+        			}
+        			
+        			this.integralImage[i][j] = leftSum + topSum - diagSum + image[i][j]; // tabulation into the bottom right
         		}
         }
-        this.integralImage = image.clone();
+       
     }
 
     /**
@@ -52,12 +82,19 @@ public class IntegralImage {
      * @throws NullSubImageException if top > bottom or left > right
      */
     public double meanSubImage(int top, int bottom, int left, int right) throws BoundaryViolationException, NullSubImageException {
+    		// exception handling
     		if ((!(top <= bottom)) || !(left <= right)){		// exception check
     			throw new NullSubImageException();
     		}
     		if ((top < 0) || (bottom > this.imageHeight) || (left < 0) || (right > this.imageWidth)){
     			throw new BoundaryViolationException();		// exception check
     		}
-    		return 0; //dummy value - remove once coded.
+    		
+    		// compute sum and samples (i.e. pixels), return average
+    		int samples = (bottom - top + 1) * (right - left + 1);
+    		
+            double sum = this.integralImage[top-1][left-1] - this.integralImage[top-1][right] - this.integralImage[bottom][left-1] + this.integralImage[bottom][right];
+    		double average = sum / samples;
+    		return average; 
     }
 }
